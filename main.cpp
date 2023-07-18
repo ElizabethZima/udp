3176f2717978dfe4ae8081634b6b5400256710a2
Студент <eliza2017.zimina@mail.ru>
2023-07-18 11:24:42 +0300

    add updreceiver

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
+#include <QByteArray>
 #include <QCoreApplication>
-#include <QDebug>
+#include <QHostAddress>
+#include <QUdpSocket>
+#include <iostream>
+#include "udpreceiver.h"
+const quint16 PORT = 2333;
 
-int main(int argc, char *argv[]) {
+
+int main(int argc, char *argv[]){
+
     QCoreApplication a(argc, argv);
-    qDebug() << "Hello World";
-    return QCoreApplication::exec();
+
+    QUdpSocket qus;
+
+    qus.bind(QHostAddress("127.0.0.1"), PORT+1);
+
+    QByteArray msg = "Hello world!";
+
+    std::cout << "--- Sender ---" << std::endl;
+
+    for(int i = 0; i < 100; ++i)
+        qus.writeDatagram(msg, QHostAddress("127.0.0.1"), PORT);
+
+    UdpReceiver ur;
+
+
+    std::cout << "--- Recevier ---" << std::endl;
+
+    return a.exec();
+
 }
---
+++ udpreceiver.cpp
@@ -0,0 +1,39 @@
+#include "udpreceiver.h"
+#include <QByteArray>
+#include <iostream>
+
+const quint16 PORT = 2333;
+
+UdpReceiver::UdpReceiver(QObject *p) :
+
+QObject(p){
+
+        uSocket = new QUdpSocket;
+
+        uSocket->bind(QHostAddress("127.0.0.1"), PORT);
+
+        connect(uSocket, SIGNAL(readyRead()), this, SLOT(receive()));
+}
+        UdpReceiver::~UdpReceiver(){
+
+    delete uSocket;
+}
+
+
+    void UdpReceiver::receive(){
+
+    QByteArray ba;
+
+    while(uSocket->hasPendingDatagrams())
+
+    {
+
+        qDebug() << "In";
+        ba.resize(uSocket->pendingDatagramSize());
+
+        uSocket->readDatagram(ba.data(), ba.size());
+
+        std::cout << ba.data() << std::endl;
+
+    }
+    }
